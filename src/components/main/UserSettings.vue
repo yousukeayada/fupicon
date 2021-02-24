4854cb85852267a131511d684258d6cafc764808
綾田陽介 <202x102x@stu.kobe-u.ac.jp>
2021-02-25 04:27:51 +0900

    ユーザ設定画面を実装

---
+++ src/components/main/UserSettings.vue
@@ -0,0 +1,114 @@
+<template>
+<div>
+    <v-list-item @click.stop="openDialog">
+        <v-list-item-title>ユーザ設定</v-list-item-title>
+    </v-list-item>
+
+    <v-dialog v-model="dialog">
+        <v-card class="p-3">
+            <v-card-title>ユーザ設定</v-card-title>
+            <v-text-field type="text" label="名前" v-model="username" prepend-icon="mdi-rename-box" />
+            <v-text-field type="email" label="メールアドレス" v-model="mailaddress" prepend-icon="mdi-email" />
+            <v-card-actions>
+                <v-btn @click="updateUser">変更</v-btn>
+            </v-card-actions>
+
+            <v-divider></v-divider>
+
+            <v-card-text>アカウントを削除する</v-card-text>
+            <v-card-actions>
+                <v-btn @click.stop="openDeleteDialog">削除</v-btn>
+            </v-card-actions>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+                <v-btn text color="primary" @click.stop="dialog=false">閉じる</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="deleteDialog">
+        <v-card class="p-3">
+            <v-card-title>アカウントを削除しますか？</v-card-title>
+            <v-card-actions>
+                <v-btn @click.stop="deleteDialog=false">キャンセル</v-btn>
+                <v-btn @click="deleteUser" class="red accent-2">削除</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+    data() {
+        return {
+            dialog: false,
+            deleteDialog: false,
+            username: "",
+            mailaddress: "",
+        }
+    },
+    created() {
+        
+    },
+    methods: {
+        openDialog() {
+            let self = this
+            let user = firebase.auth().currentUser;
+            if(user) {
+                let database = firebase.database();
+                database.ref("/users/"+user.uid+"/username").once("value", function(data) {
+                    self.username = data.val()
+                })
+                self.mailaddress = user.email
+                self.password = user.providerData[0].providerId
+                this.dialog = true
+            } else {
+                alert("サインインしてください")
+            }
+        },
+        updateUser() {
+            let self = this
+            let user = firebase.auth().currentUser;
+            if(user) {
+                let database = firebase.database();
+                database.ref("/users/"+user.uid+"/").update({
+                    username: self.username
+                })
+                user.updateEmail(self.mailaddress).then(function() {
+
+                }).catch(function(error) {
+                    alert(error)
+                });
+                alert("アカウント情報を変更しました："+this.username)
+            } else {
+                alert("サインインしてください")
+            }
+        },
+        openDeleteDialog() {
+            this.deleteDialog = true
+        },
+        deleteUser() {
+            let self = this
+            let user = firebase.auth().currentUser;
+            if(user) {
+                let database = firebase.database();
+                database.ref("/users/"+user.uid+"/").remove()
+                user.delete().then(function() {
+                    alert("アカウントを削除しました")
+                    self.$router.push('/signin')
+                }).catch(function(error) {
+                    alert(error)
+                })
+            } else {
+                alert("サインインしてください")
+            }
+        },
+    
+    }
+}
+</script>
--- src/components/util/Header.vue
+++ src/components/util/Header.vue
@@ -1,10 +1,18 @@
 <template>
 <div>
-    <v-navigation-drawer app clipped v-model="drawer" class="indigo lighten-1">
-        <div class="text-center text-white">
-        <h4 class="m-3">Navigation List</h4>
-        <v-list>
+    <v-navigation-drawer app clipped v-model="drawer" class="indigo lighten-5">
+        <div class=" text-grey">
+        <h4 class="m-3">Menu</h4>
+        <v-list nav>
+            <v-list-item-group>
+
+                <DiscordConnect></DiscordConnect>
+
+                <LineConnect></LineConnect>
+
+                <UserSettings></UserSettings>
     
+            </v-list-item-group>
             <v-list-item>
                 <SignoutButton></SignoutButton>
             </v-list-item>
@@ -23,10 +31,16 @@
 
 <script>
 import SignoutButton from '@/components/auth/SignoutButton'
+import DiscordConnect from '@/components/main/DiscordConnect'
+import LineConnect from '@/components/main/LineConnect'
+import UserSettings from '@/components/main/UserSettings'
 
 export default {
     components: {
         SignoutButton,
+        DiscordConnect,
+        LineConnect,
+        UserSettings,
     },
     data() {
         return {
@@ -42,5 +56,9 @@ export default {
 .h1, h1 {
   font-family: 'Cabin Sketch', cursive;
   /* font-family: 'Chewy', cursive; */
+}
+
+.h4, h4 {
+  font-family: 'Cabin Sketch', cursive;
 }
 </style>
